1ac5ada4641f91ed6c4c9f5e5282d37bd89b8602
Yeon Jeffrey <yeonjeseo@hotmail.com>
2024-04-08 18:42:55 +0900

    2024.04.08

---
+++ volB/chap5/seo.dart
@@ -0,0 +1,16 @@
+var longText = "Hello, "
+    "my "
+    "name "
+    "is "
+    "Yeonje "
+    "Seo";
+
+String longText2 = '''
+  Hello. This is an apple.
+  And dart is awesome!
+Indent included.
+''';
+void main() {
+  print(longText);
+  print(longText2);
+}
