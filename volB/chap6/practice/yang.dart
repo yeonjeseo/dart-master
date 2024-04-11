7e9b2ce4e5aac45445b4eb339d9bf636fd96e3cf
Yang⭐ <78190786+313yang@users.noreply.github.com>
2024-04-11 23:48:56 +0900

    chap6 예제추가

---
+++ volB/chap6/practice/yang.dart
@@ -0,0 +1,179 @@
+void print1dan() {
+  var result = '';
+
+  result += ("1 x 1 = ${1 * 1}\n");
+  result += ("1 x 2 = ${1 * 2}\n");
+  result += ("1 x 3 = ${1 * 3}\n");
+  result += ("1 x 4 = ${1 * 4}\n");
+  result += ("1 x 5 = ${1 * 5}\n");
+  result += ("1 x 6 = ${1 * 6}\n");
+  result += ("1 x 7 = ${1 * 7}\n");
+  result += ("1 x 8 = ${1 * 8}\n");
+  result += ("1 x 9 = ${1 * 9}\n");
+  result += ("--------------\n");
+  return print(result);
+}
+
+void print2dan() {
+  var result = '';
+
+  result += ("2 x 1 = ${2 * 1}\n");
+  result += ("2 x 2 = ${2 * 2}\n");
+  result += ("2 x 3 = ${2 * 3}\n");
+  result += ("2 x 4 = ${2 * 4}\n");
+  result += ("2 x 5 = ${2 * 5}\n");
+  result += ("2 x 6 = ${2 * 6}\n");
+  result += ("2 x 7 = ${2 * 7}\n");
+  result += ("2 x 8 = ${2 * 8}\n");
+  result += ("2 x 9 = ${2 * 9}\n");
+  result += ("--------------\n");
+  return print(result);
+}
+
+void print3dan() {
+  var result = '';
+
+  result += ("3 x 1 = ${3 * 1}\n");
+  result += ("3 x 2 = ${3 * 2}\n");
+  result += ("3 x 3 = ${3 * 3}\n");
+  result += ("3 x 4 = ${3 * 4}\n");
+  result += ("3 x 5 = ${3 * 5}\n");
+  result += ("3 x 6 = ${3 * 6}\n");
+  result += ("3 x 7 = ${3 * 7}\n");
+  result += ("3 x 8 = ${3 * 8}\n");
+  result += ("3 x 9 = ${3 * 9}\n");
+  result += ("--------------\n");
+  return print(result);
+}
+
+void print4dan() {
+  var result = '';
+
+  result += ("4 x 1 = ${4 * 1}\n");
+  result += ("4 x 2 = ${4 * 2}\n");
+  result += ("4 x 3 = ${4 * 3}\n");
+  result += ("4 x 4 = ${4 * 4}\n");
+  result += ("4 x 5 = ${4 * 5}\n");
+  result += ("4 x 6 = ${4 * 6}\n");
+  result += ("4 x 7 = ${4 * 7}\n");
+  result += ("4 x 8 = ${4 * 8}\n");
+  result += ("4 x 9 = ${4 * 9}\n");
+  result += ("--------------\n");
+  return print(result);
+}
+
+void print5dan() {
+  var result = '';
+
+  result += ("5 x 1 = ${5 * 1}\n");
+  result += ("5 x 2 = ${5 * 2}\n");
+  result += ("5 x 3 = ${5 * 3}\n");
+  result += ("5 x 4 = ${5 * 4}\n");
+  result += ("5 x 5 = ${5 * 5}\n");
+  result += ("5 x 6 = ${5 * 6}\n");
+  result += ("5 x 7 = ${5 * 7}\n");
+  result += ("5 x 8 = ${5 * 8}\n");
+  result += ("5 x 9 = ${5 * 9}\n");
+  result += ("--------------\n");
+  return print(result);
+}
+
+void print6dan() {
+  var result = '';
+
+  result += ("6 x 1 = ${6 * 1}\n");
+  result += ("6 x 2 = ${6 * 2}\n");
+  result += ("6 x 3 = ${6 * 3}\n");
+  result += ("6 x 4 = ${6 * 4}\n");
+  result += ("6 x 5 = ${6 * 5}\n");
+  result += ("6 x 6 = ${6 * 6}\n");
+  result += ("6 x 7 = ${6 * 7}\n");
+  result += ("6 x 8 = ${6 * 8}\n");
+  result += ("6 x 9 = ${6 * 9}\n");
+  result += ("--------------\n");
+  return print(result);
+}
+
+void print7dan() {
+  var result = '';
+
+  result += ("7 x 1 = ${7 * 1}\n");
+  result += ("7 x 2 = ${7 * 2}\n");
+  result += ("7 x 3 = ${7 * 3}\n");
+  result += ("7 x 4 = ${7 * 4}\n");
+  result += ("7 x 5 = ${7 * 5}\n");
+  result += ("7 x 6 = ${7 * 6}\n");
+  result += ("7 x 7 = ${7 * 7}\n");
+  result += ("7 x 8 = ${7 * 8}\n");
+  result += ("7 x 9 = ${7 * 9}\n");
+  result += ("--------------\n");
+  return print(result);
+}
+
+void print8dan() {
+  var result = '';
+
+  result += ("8 x 1 = ${8 * 1}\n");
+  result += ("8 x 2 = ${8 * 2}\n");
+  result += ("8 x 3 = ${8 * 3}\n");
+  result += ("8 x 4 = ${8 * 4}\n");
+  result += ("8 x 5 = ${8 * 5}\n");
+  result += ("8 x 6 = ${8 * 6}\n");
+  result += ("8 x 7 = ${8 * 7}\n");
+  result += ("8 x 8 = ${8 * 8}\n");
+  result += ("8 x 9 = ${8 * 9}\n");
+  result += ("--------------\n");
+  return print(result);
+}
+
+void print9dan() {
+  var result = '';
+
+  result += ("9 x 1 = ${9 * 1}\n");
+  result += ("9 x 2 = ${9 * 2}\n");
+  result += ("9 x 3 = ${9 * 3}\n");
+  result += ("9 x 4 = ${9 * 4}\n");
+  result += ("9 x 5 = ${9 * 5}\n");
+  result += ("9 x 6 = ${9 * 6}\n");
+  result += ("9 x 7 = ${9 * 7}\n");
+  result += ("9 x 8 = ${9 * 8}\n");
+  result += ("9 x 9 = ${9 * 9}\n");
+  result += ("--------------");
+  return print(result);
+}
+
+void main() {
+  var dan = 12;
+  switch (dan) {
+    case 1:
+      print1dan();
+      break;
+    case 2:
+      print2dan();
+      break;
+    case 3:
+      print3dan();
+      break;
+    case 4:
+      print4dan();
+      break;
+    case 5:
+      print5dan();
+      break;
+    case 6:
+      print6dan();
+      break;
+    case 7:
+      print7dan();
+      break;
+    case 8:
+      print8dan();
+      break;
+    case 9:
+      print9dan();
+      break;
+    default:
+      print("1~9단 밖에 없지롱");
+      break;
+  }
+}
