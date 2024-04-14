1b8a8123889560f827c83475090a6e79fbc4aca0
Yeon Jeffrey <yeonjeseo@hotmail.com>
2024-04-15 00:17:06 +0900

    chapter6 assert

---
+++ volB/chap6/seo.dart
@@ -0,0 +1,8 @@
+void main() {
+  print(1 == 1);
+
+  // assert 내 판별식이 true가 아니면 프로그램 종료
+  // assert는 프로그램에서 예상하지 못한 상황을 만나는 경우에 대한 처리를 프로그래밍하는 문법
+  var programmingTermination = "NORMAL";
+  assert(programmingTermination != "NORMAL");
+}
